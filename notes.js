6d4aa16c8d358c862152e03b77eee5c950eb3a1e
Hil Patel <hilrpatel5678@gmail.com>
2024-07-10 18:38:28 +0530

    Initial commit

---
+++ notes.js
@@ -0,0 +1,3 @@
+// Dev Dependencies are those dependencies that are only useful during development phase.
+// Once deployed it doesnt serve any purpose
+// npm i --save-dev nodemon to install nodemon as a dev dependency
